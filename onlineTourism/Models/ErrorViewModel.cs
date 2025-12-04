dce1141b7e407872df71501b30506abd746d623f
sachinthapa <20028232@students.koi.edu.au>
2025-12-04 21:27:36 +1100

    Setup the Template

---
+++ onlineTourism/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace onlineTourism.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
